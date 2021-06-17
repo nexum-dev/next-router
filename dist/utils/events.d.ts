d35483112b6b6966881c304308ca3ac3b14f5b30
drdrej <andrej.siebert@gmail.com>
2021-06-17 08:55:06 +0200

    fix/PIC-5858 add dist to ignore

--- dist/utils/events.d.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'Router/Router';
-declare type Handler = (...evts: any[]) => void;
-export declare type Events = {
-    on(type: string, handler: Handler): void;
-    off(type: string, handler: Handler): void;
-};
-export default function events(router: Router): Events;
-export {};
--- dist/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getUrlParams: (queryString: string) => {
-    [key: string]: string | null;
-};
